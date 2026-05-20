09f1e565339c562386b2d82bd43546b25e7bf472
Brian Collier <blcollier@gmail.com>
2026-05-20 16:36:20 -0500

    start of token creation endpoints 2

--- app/api/token/create/route.ts
+++ app/api/token/create/route.ts
@@ -8,17 +8,20 @@ export const dynamic = 'force-dynamic';
 const supabase = createClient();
 
 //TEST
-const n8n_endpoint= "http://n8n.abundancedigitalmedia.com:5678/webhook-test/d561cf1b-27ba-4f65-ad21-77df991648ee"
+const N8N_WEBHOOK = "http://n8n.abundancedigitalmedia.com:5678/webhook-test/d561cf1b-27ba-4f65-ad21-77df991648ee"
 
 //PRODUCTION
-//const n8n_endpoint = "http://n8n.abundancedigitalmedia.com:5678/webhook/d561cf1b-27ba-4f65-ad21-77df991648ee"
+//const N8N_WEBHOOK = "http://n8n.abundancedigitalmedia.com:5678/webhook/d561cf1b-27ba-4f65-ad21-77df991648ee "
 
+const AWS_CREDENTIALS  ='';
+const AWS_ENDPOINT = '';
 
 export async function POST(request: Request) {
     const body = await request.json();
 
     try {
-        const uploadResponse = uploadTokenMeta(body.token);
+        const uploadTokenMetaResponse = uploadTokenMeta(body.token);
+        const uploadTokenIMageREsponse = 
     } catch (error) {
         return new Response(), {
             headers: { 'Content-Type': 'application/json' },
@@ -32,7 +35,21 @@ export async function POST(request: Request) {
 
 
 async function uploadTokenMeta(token: {}) {
-    const response = await fetch(n8n_endpoint, {
+    const response = await fetch(N8N_WEBHOOK, {
+        method: 'PUT',
+        headers: {
+        'Content-Type': 'application/json',
+        // 'Authorization': `Bearer ${process.env.API_TOKEN}`, // Securely use env vars
+        },
+        body: JSON.stringify(token),
+    });
+
+    if (!response.ok) throw new Error('Failed to update');
+    return response.json();
+}
+
+async function uploadTokenImage() {
+    const response = await fetch(AWS_ENDPOINT, {
         method: 'PUT',
         headers: {
         'Content-Type': 'application/json',
