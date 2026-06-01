946c7f14a64b8079d0f92ad173d692fd58dcf478
bcz5678 <blcollier@gmail.com>
2026-06-01 07:50:32 -0500

    pre SF

--- app/api/pumpfun/token-info/route.ts
+++ app/api/pumpfun/token-info/route.ts
@@ -1,31 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
 import BN from 'bn.js';
+import { PublicKey } from "@solana/web3.js";
 
 import { getTokenSnapshot } from '@/lib/pumpfun/token-snapshot';
 import { TokenSnapshot } from '@/lib/types/token-pumpfun';
 
-export async function POST(request: NextRequest) {
+
+export async function GET(request: NextRequest) {   
+    const url = new URL(request.url);
+    const mintAddress = url.searchParams.get('mintAddress');
    
 
     try{
-        const body = await request.json();
-        const tokenSnapshotResponse: TokenSnapshot | null = await getTokenSnapshot(body.mintAddress); 
-     
-        if (tokenSnapshotResponse != null) {
-            return NextResponse.json({
-                message: "Token Snapshot",
-                body: {
-                    snapshot: tokenSnapshotResponse,
-                }
-            },{
-                status: 200    
-            });
+        
+        if(mintAddress != null){
+            const tokenSnapshotResponse: TokenSnapshot | null = await getTokenSnapshot(new PublicKey(mintAddress)); 
+        
+            if (tokenSnapshotResponse != null) {
+                return NextResponse.json({
+                    message: "Token Snapshot",
+                    body: {
+                        snapshot: tokenSnapshotResponse,
+                    }
+                },{
+                    status: 200    
+                });
+            } else {
+                return NextResponse.json({ error: 'Unable to return snapshot' }, { status: 400 })
+            }
         } else {
-             return NextResponse.json({ error: 'Unable to return snapshot' }, { status: 400 })
+           return NextResponse.json({ error: 'Invalid Request' }, { status: 400 })
         }
+       
 
     } catch {
-        return NextResponse.json({ error: 'Invalid JSON body' }, { status: 400 })
+        return NextResponse.json({ error: 'Invalid Request' }, { status: 400 })
     }
 }
 
